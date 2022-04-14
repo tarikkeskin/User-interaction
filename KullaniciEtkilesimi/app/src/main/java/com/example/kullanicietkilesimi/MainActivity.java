03fc7a2c1d737230b874094bbbcfd5c4e7c7ca32
tarikkeskin <tarik.keskin007@gmail.com>
2022-04-14 22:15:00 +0300

    ViewBinding

--- KullaniciEtkilesimi/app/src/main/java/com/example/kullanicietkilesimi/MainActivity.java
+++ KullaniciEtkilesimi/app/src/main/java/com/example/kullanicietkilesimi/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.kullanicietkilesimi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+
+import com.example.kullanicietkilesimi.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding tasarim;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        tasarim = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(tasarim.getRoot());
+
+
     }
 }
