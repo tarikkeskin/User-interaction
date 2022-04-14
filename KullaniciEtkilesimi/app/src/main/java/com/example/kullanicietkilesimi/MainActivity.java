4a11c8e2a8bd8de2e755925af3d0eb00043f24d9
tarikkeskin <tarik.keskin007@gmail.com>
2022-04-14 22:47:52 +0300

    Toast,snackbar,alert

--- KullaniciEtkilesimi/app/src/main/java/com/example/kullanicietkilesimi/MainActivity.java
+++ KullaniciEtkilesimi/app/src/main/java/com/example/kullanicietkilesimi/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.kullanicietkilesimi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.kullanicietkilesimi.databinding.ActivityMainBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +19,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         tasarim = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(tasarim.getRoot());
+        
+        
+        tasarim.buttonToast.setOnClickListener(view -> {
+            Toast.makeText(this, "Merhaba", Toast.LENGTH_SHORT).show();
+        });
+        
+        tasarim.buttonSnackbar.setOnClickListener(view -> {
+            Snackbar.make(view,"silmek istiyor musun ?",Snackbar.LENGTH_SHORT)
+                    .setAction("Evet",view1 -> {
+                        Snackbar.make(view1,"Silindi!!",Snackbar.LENGTH_SHORT)
+                                .setTextColor(Color.BLUE)
+                                .setBackgroundTint(Color.WHITE)
+                                .show();
+                    })
+                    .setActionTextColor(Color.RED)
+                    .setTextColor(Color.BLUE)
+                    .setBackgroundTint(Color.WHITE)
+                    .show();
+        });
+        
+        tasarim.buttonAlert.setOnClickListener(view -> {
+            AlertDialog.Builder ad = new AlertDialog.Builder(this);
+            ad.setTitle("Başlık");
+            ad.setMessage("İçerik");
 
+            ad.setPositiveButton("Tamam",(d,i) -> {
+                // d -> dialog interface , button itself
+                Toast.makeText(this, "Tamam seçildi", Toast.LENGTH_SHORT).show();
+            });
+            ad.setNegativeButton("Iptal",(d,i) -> {
+                Toast.makeText(this, "İptal seçildi", Toast.LENGTH_SHORT).show();
+            });
 
+            ad.create().show();
+        });
     }
 }
